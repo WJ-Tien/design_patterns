bece70f8b5a3773a46c7455085ceac62f0b8b8a8
WJ-Tien <chc61394@gmail.com>
2024-04-25 16:29:42 +0800

    abs factory

---
+++ abstract_factory/abstract_factory.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+
+class CarFactory(ABC):
+    @abstractmethod
+    def boost(self):
+        pass
+
+    @abstractmethod
+    def brake(self):
+        pass
+
+    @abstractmethod
+    def drift(self):
+        pass
+
+
+class CarA(CarFactory):
+    def boost(self):
+        print("CarA boosts")
+
+    def brake(self):
+        print("CarA brakes")
+
+    def drift(self):
+        print("CarA drifts")
+
+
+class CarB(CarFactory):
+    def boost(self):
+        print("CarB boosts")
+
+    def brake(self):
+        print("CarB brakes")
+
+    def drift(self):
+        print("CarB drifts")
